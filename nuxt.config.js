23b094cf93cf2cca79b7b964c591b26699eacdc3
Oliver Lehner <oliver.lehner@grandgarage.eu>
2023-05-26 10:30:01 +0200

    Testing if empty CSS variables are removed from the final CSS file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import { join } from 'path'
 const axios = require('axios')
 // TODO: use functions for keys needed while runtime (https://answers.netlify.com/t/support-guide-how-do-i-keep-my-api-keys-tokens-safe-using-netlify-functions/293)
 const storyblokToken = '1IsgW07t4t5sm0UzdHAD6gtt'
@@ -61,7 +60,6 @@ module.exports = {
   },
   buildModules: [
     ['storyblok-nuxt', { accessToken: storyblokToken, cacheProvider: 'memory' }],
-    ['@storyblok/nuxt-2/module', { accessToken: storyblokToken, apiOptions: { cache: { type: 'memory' } } }],
     '@nuxtjs/proxy',
     ['@nuxtjs/google-analytics'],
     ['@nuxtjs/style-resources']
@@ -170,6 +168,19 @@ module.exports = {
    ** Build configuration
    */
   build: {
+    optimizeCSS: true,
+    postcss: {
+      plugins: {
+        cssnano: {
+          preset: [
+            'default',
+            {
+              calc: false
+            }
+          ]
+        }
+      }
+    },
     transpile: [/^vue2-google-maps($|\/)/]
   },
   googleAnalytics: {
