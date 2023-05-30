96e79c302718413a5d9e8fdea9f7fbcdc5a738ac
Oliver Lehner <oliver.lehner@grandgarage.eu>
2023-05-30 14:14:08 +0200

    added preview of generated site

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,6 +104,7 @@ module.exports = {
     '~/plugins/routersync',
     '~/plugins/fontawesome.js',
     '~/plugins/getActiveBreakpoint.client.js',
+    '~/plugins/preview.client.js',
     //'~/plugins/snow.js',
     { src: '~/plugins/components-nossr', ssr: false }
   ],
@@ -113,8 +114,9 @@ module.exports = {
   router: {
     middleware: 'router'
   },
+  target: 'static',
   generate: {
-    target: 'static',
+    //target: 'static',
     routes: function (callback) {
       const token = storyblokToken
       const perPage = 100
