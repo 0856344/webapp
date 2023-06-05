7deabb9711719cbac9a0f4a4980d1a7fd654b6c4
Oliver Lehner <oliver.lehner@grandgarage.eu>
2023-06-05 12:09:43 +0200

    Membership selection [HP-267]

--- store/index.js
+++ store/index.js
@@ -984,7 +984,6 @@ const createStore = () => {
           cv: state.cacheVersion
         })
           .then((res) => {
-            console.log('getDataSource', res)
             return res.data
           }).catch((res) => {
             this.$sentry.captureException(res)
