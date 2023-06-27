463ac360815758b6c00570026a678f979f541672
0856344 <Sebastian.Kaltenboeck@gmx.at>
2023-06-27 09:11:59 +0200

    notes -> metadata, style fixes

--- pages/wizard/onboarding/confirmation.vue
+++ pages/wizard/onboarding/confirmation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section">
-    <div style="margin-top: 2em;"><img width="80px" src="~/assets/img/icons/fireworks.svg" class="decorator"></div>
+    <div style="margin-top: 2em"><img width="80px" src="~/assets/img/icons/fireworks.svg" class="center"></div>
     <h2 style="margin: 0;">{{ $t('congrats') }}</h2>
     <p class="text" style="margin-bottom: 0">
       {{ $t('youMadeTheFirstStepsTowardsBecomingA-GG-Member') }}<br><br>{{ $t('toCompleteOnboardingStartSafetyTraining') }}
@@ -9,10 +9,8 @@
 </template>
 <script>
 export default {
-  //middleware: 'authenticated',
   mounted () {
     window.scrollTo(0, 0)
-    //this.$store.dispatch('getMemberCourses')
   }
 }
 </script>
@@ -21,5 +19,10 @@ export default {
   text-align: center;
   margin-top: 1em;
   margin-bottom: 0;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
