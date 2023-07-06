8aa4fee8823d461af00d1ad79c452971233814a3
Oliver Lehner <oliver.lehner@grandgarage.eu>
2023-07-06 11:18:08 +0200

    Made CenteredText component actually center text

--- bloks/basic/CenteredText.vue
+++ bloks/basic/CenteredText.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-editable="blok"
-    class="centered-text"
-  >
+  <div v-editable="blok" class="p-4 lg:p-8 text-center text-blue lg:text-3xl text-xl leading-normal">
     <p>{{ blok.text }}</p>
   </div>
 </template>
@@ -12,18 +9,3 @@ export default {
   props: ['blok']
 }
 </script>
-
-<style lang="scss">
-
-.centered-text {
-  padding: 2rem;
-  font-family: 'Chakra Petch',serif;
-  color: $color-blue;
-  font-size: 2rem;
-  line-height: 1.5;
-  @include media-breakpoint-down(sm) {
-    font-size: 1.2rem;
-    padding: 5%;
-  }
-}
-</style>
