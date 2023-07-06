f1f01a66934c8ac4978ee741e4dc6483015aa4a8
murcoder <chris.murauer@yahoo.de>
2023-07-06 14:33:04 +0200

    https://grandgarage.atlassian.net/browse/HP-270 * fixed build warnings * styled modal * added missing tailwind classes

--- pages/me/bookings.vue
+++ pages/me/bookings.vue
@@ -13,16 +13,17 @@
       </modal>
       <v-tour name="myTour" :steps="steps"></v-tour>
       <div class="flex items-center mb-1">
-        <h2 class="m-0 mr-1 text-2xl">
+        <h2 class="m-0 mr-2 text-2xl">
           {{ $t("machineBookings") }}
           <svg
-            class="cursor-pointer icon-button inline-block fill-current w-4 h-4"
+            class="cursor-pointer icon-button inline-block fill-current w-5 h-5 ml-1"
             xmlns="http://www.w3.org/2000/svg"
+            height="1em"
             @click="startTour"
             viewBox="0 0 512 512"
           >
             <path
-              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
+              d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
             />
           </svg>
         </h2>
@@ -87,13 +88,13 @@
               placeholder="Maschine wählen"
             >
             </v-select>
-            <span id="v-step-2" class="v-step-3" v-if="selectedMachine">
+            <span v-if="selectedMachine" id="v-step-2" class="v-step-3">
               <machine-calendar
                 class="pt-6"
                 :resource="this.selectedMachine.id"
               ></machine-calendar>
             </span>
-            <div class="flex justify-end">
+            <div v-if="selectedMachine" class="flex justify-end">
               <button
                 class="input-button-primary v-step-4 shadow-md"
                 @click="openModal"
@@ -167,7 +168,7 @@ export default {
         {
           target: ".v-step-3",
           content:
-            "<b>Reservierung löschen</b> <br><hr class='m-1'>Halte die Maustaste gedrückt, um einen, noch nicht bestätigten, Termin wieder zu entfernen.",
+            "<b>Reservierung löschen</b> <br><hr class='m-1'>Halte die Maustaste am gewünschten Zeitslot gedrückt, um einen Termin wieder zu entfernen.",
           offset: -300,
           background: "#000",
         },
