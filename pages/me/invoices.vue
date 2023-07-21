d48de80995c49d152046721272d40c5b26f4d6c2
0856344 <Sebastian.Kaltenboeck@gmx.at>
2023-07-21 11:05:18 +0200

    hide cancelled invoices

--- pages/me/invoices.vue
+++ pages/me/invoices.vue
@@ -1,83 +1,124 @@
 <template>
-    <div>
-        <section>
-            <div class="flex items-center mb-1">
-                <h2 class="m-0 mr-1 text-2xl">Offene Buchungen</h2>
-                <loading-spinner-inline v-if="loadingActivities"/>
-            </div>
-            <div v-if="activities">
-                <table v-if="activities.length > 0" class="member-portal-table table-auto">
-                    <thead>
-                    <tr>
-                        <th class="activity-date">Datum</th>
-                        <th class="activity-amount">Betrag</th>
-                        <th class="activity-description">Buchung</th>
-                        <th class="activity-status"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="activity of activities" :key="activity.id">
-                        <td class="activity-date">{{ new Date(activity.date).toLocaleDateString('de-AT') }}</td>
-                        <td class="activity-amount" :style="{color: activity.price > 0 ? 'red' : 'green'}">
-                            {{ Number(activity.price * (-1)).toFixed(2).replace('.', ',') }} €
-                        </td>
-                        <td class="activity-description">{{ activity.description }}</td>
-                        <td class="activity-status"></td>
-                    </tr>
-                    <tr class="activity-total">
-                        <td></td>
-                        <td></td>
-                        <td class="result"><b>{{ totalResult > 0 ? 'Gesamt:' : 'Guthaben:' }}</b></td>
-                        <td class="total" :style="{color: totalResult > 0 ? 'red' : 'green'}">
-                            {{ (Number(totalResult * (-1)).toFixed(2).replace('.', ',')) }} €
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div v-else><p>Keine Buchungen vorhanden.</p></div>
-            </div>
-        </section>
+  <div>
+    <section>
+      <div class="flex items-center mb-1">
+        <h2 class="m-0 mr-1 text-2xl">Offene Buchungen</h2>
+        <loading-spinner-inline v-if="loadingActivities" />
+      </div>
+      <div v-if="activities">
+        <table
+          v-if="activities.length > 0"
+          class="member-portal-table table-auto"
+        >
+          <thead>
+            <tr>
+              <th class="activity-date">Datum</th>
+              <th class="activity-amount">Betrag</th>
+              <th class="activity-description">Buchung</th>
+              <th class="activity-status"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="activity of activities" :key="activity.id">
+              <td class="activity-date">
+                {{ new Date(activity.date).toLocaleDateString("de-AT") }}
+              </td>
+              <td
+                class="activity-amount"
+                :style="{ color: activity.price > 0 ? 'red' : 'green' }"
+              >
+                {{
+                  Number(activity.price * -1)
+                    .toFixed(2)
+                    .replace(".", ",")
+                }}
+                €
+              </td>
+              <td class="activity-description">{{ activity.description }}</td>
+              <td class="activity-status"></td>
+            </tr>
+            <tr class="activity-total">
+              <td></td>
+              <td></td>
+              <td class="result">
+                <b>{{ totalResult > 0 ? "Gesamt:" : "Guthaben:" }}</b>
+              </td>
+              <td
+                class="total"
+                :style="{ color: totalResult > 0 ? 'red' : 'green' }"
+              >
+                {{
+                  Number(totalResult * -1)
+                    .toFixed(2)
+                    .replace(".", ",")
+                }}
+                €
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div v-else><p>Keine Buchungen vorhanden.</p></div>
+      </div>
+    </section>
 
-        <section class="invoice">
-            <div class="flex items-center mb-1">
-                <h2 class="m-0 mr-1 text-2xl">Rechnungen</h2>
-                <loading-spinner-inline v-if="loadingInvoices"/>
-            </div>
-            <div v-if="invoices">
-                <table v-if="invoices.length > 0" class="member-portal-table table-auto">
-                    <thead>
-                    <tr>
-                        <th class="invoice-date">Datum</th>
-                        <th class="invoice-number">Rechnung</th>
-                        <th class="invoice-amount">Betrag</th>
-                        <th class="invoice-status">Status</th>
-                        <th class="invoice-download"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="invoice of invoices" :key="invoice.id">
-                        <td class="invoice-date">{{ new Date(invoice.date).toLocaleDateString('de-AT') }}</td>
-                        <td class="invoice-number">{{ invoice.number }}</td>
-                        <td class="invoice-amount">{{ invoice.total.replace('.', ',') }} €</td>
-                        <td class="invoice-status">
-                            <div v-if="invoice.state" class="bubble" :class="getInvoiceStateClass(invoice)">
-                                {{ getInvoiceStateText(invoice) }}
-                            </div>
-                        </td>
-                        <td @click="getPdf(invoice)" class="invoice-download">
-                            <div class="text-center">
-                                <loading-spinner v-if="loadingPdf" color="#333"
-                                                 class="pdf-loading-spinner flex text-center justify-center items-center"/>
-                                <font-awesome-icon v-else icon="download" class="clickable-icon"/>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div v-else><p>Keine Rechnungen vorhanden.</p></div>
-            </div>
-        </section>
-    </div>
+    <section class="invoice">
+      <div class="flex items-center mb-1">
+        <h2 class="m-0 mr-1 text-2xl">Rechnungen</h2>
+        <loading-spinner-inline v-if="loadingInvoices" />
+      </div>
+      <div v-if="invoices">
+        <table
+          v-if="invoices.length > 0"
+          class="member-portal-table table-auto"
+        >
+          <thead>
+            <tr>
+              <th class="invoice-date">Datum</th>
+              <th class="invoice-number">Rechnung</th>
+              <th class="invoice-amount">Betrag</th>
+              <th class="invoice-status">Status</th>
+              <th class="invoice-download"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="invoice of invoices" :key="invoice.id">
+              <td class="invoice-date">
+                {{ new Date(invoice.date).toLocaleDateString("de-AT") }}
+              </td>
+              <td class="invoice-number">{{ invoice.number }}</td>
+              <td class="invoice-amount">
+                {{ invoice.total.replace(".", ",") }} €
+              </td>
+              <td class="invoice-status">
+                <div
+                  v-if="invoice.state"
+                  class="bubble"
+                  :class="getInvoiceStateClass(invoice)"
+                >
+                  {{ getInvoiceStateText(invoice) }}
+                </div>
+              </td>
+              <td @click="getPdf(invoice)" class="invoice-download">
+                <div class="text-center">
+                  <loading-spinner
+                    v-if="loadingPdf"
+                    color="#333"
+                    class="pdf-loading-spinner flex text-center justify-center items-center"
+                  />
+                  <font-awesome-icon
+                    v-else
+                    icon="download"
+                    class="clickable-icon"
+                  />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div v-else><p>Keine Rechnungen vorhanden.</p></div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 export default {
@@ -98,9 +139,15 @@ export default {
 
     // Load invoices
     this.loadingInvoices = true
-    await this.$store.dispatch('getInvoices')
+    await this.$store
+      .dispatch('getInvoices')
       .then((res) => {
         this.invoices = res
+        this.invoices = this.invoices.filter((invoice) => {
+          return (
+            invoice.cancelledInvoice === null && invoice.state !== 'cancelled'
+          )
+        })
       })
       .catch((error) => {
         console.error('Error! Could not load invoices', error)
@@ -111,7 +158,8 @@ export default {
 
     // Load activities
     this.loadingActivities = true
-    await this.$store.dispatch('getActivities')
+    await this.$store
+      .dispatch('getActivities')
       .then((res) => {
         this.activities = res
       })
@@ -166,7 +214,8 @@ export default {
     },
     async getPdf (invoice) {
       this.loadingPdf = true
-      await this.$store.dispatch('getPDF', invoice.id)
+      await this.$store
+        .dispatch('getPDF', invoice.id)
         .then((res) => {
           const binary = atob(res.pdf.replace(/\s/g, ''))
           const len = binary.length
@@ -194,7 +243,6 @@ export default {
         })
     }
   }
-
 }
 </script>
 <style lang="scss" scoped>
@@ -202,11 +250,9 @@ export default {
   margin: 10% 0;
 }
 .activity-total {
-    border-top: 2px solid #333;
+  border-top: 2px solid #333;
 
-    @include media-breakpoint-down(sm) {
-
-    }
-
+  @include media-breakpoint-down(sm) {
+  }
 }
 </style>
