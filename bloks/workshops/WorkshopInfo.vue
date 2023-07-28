e83ad9b8f4262695b07d7ac0fb5403464c59d918
Oliver Lehner <oliver.lehner@grandgarage.eu>
2023-07-28 12:41:36 +0200

    [HP-239] Hides empty pretix calendar for workshops  - Adds new method to only get future events from pretix via connector

--- bloks/workshops/WorkshopInfo.vue
+++ bloks/workshops/WorkshopInfo.vue
@@ -1,6 +1,8 @@
 <template>
   <div v-editable="blok" v-if="workshopInformation" class="workshop-info">
+  <div v-editable="blok" v-if="workshopInformation" class="workshop-info">
     <div class="left-content">
+      <h2 v-if="subtitle" class="headline">
       <h2 v-if="subtitle" class="headline">
         {{ subtitle }}
       </h2>
@@ -14,13 +16,19 @@
         <div class="w-4/5 lg:w-1/2 rounded-md bg-white shadow-sm shadow-blue-700 m-4 py-1 px-2 text-center flex flex-col">
         <p class="lg:text-xl text-lg font-bold font-mono text-white bg-blue my-2">
           Danke für dein Interesse an diesem Workshop! </p>
-          <p class="text-lg lg:leading-relaxed leading-tight m-0 mb-2">Derzeit sind keine
-          Termine geplant. Melde dich bei unserem <a href="mailto:frontdesk@grandgarage.eu">Frontdesk</a> um auf dem Laufenden zu bleiben.
+          <p class="text-lg lg:leading-relaxed leading-tight m-0 mb-2">Derzeit ist kein
+          Termin geplant. Melde dich bei unserem <a href="mailto:frontdesk@grandgarage.eu">Frontdesk</a> um auf dem Laufenden zu bleiben.
         </p>
       </div>
       </div>
     </div>
     <div>
+      <component
+        v-for="i in blok.contentBloks"
+        :blok="i"
+        :is="i.component"
+        :key="i.uid"
+      />
       <component
         v-for="i in blok.contentBloks"
         :blok="i"
@@ -37,6 +45,16 @@ import { getMetaTagsForPage } from '@/services/MetaDataService'
 export default {
   props: ['blok', 'workshopInformation'],
   data () {
+    return {
+      futureEvents: []
+    }
+  },
+  created () {
+    this.$store
+      .dispatch('getFutureEvents', this.blok.pretix_shortform)
+      .then((res) => {
+        this.futureEvents = res
+      })
     return {
       futureEvents: []
     }
@@ -112,6 +130,8 @@ export default {
     }
     .teaser,
     .info-text {
+    .teaser,
+    .info-text {
       font-weight: normal;
       font-family: $font-primary;
       line-height: 1.8;
@@ -119,6 +139,7 @@ export default {
       @include media-breakpoint-down(sm) {
         line-height: 1.7;
         font-size: 1rem;
+        margin: 0 0 0 5%;
         margin: 0 0 0 5%;
       }
     }
@@ -133,6 +154,7 @@ export default {
         margin-left: 25%;
       }
       color: #fff;
+      padding: 0.7em 0.8em;
       padding: 0.7em 0.8em;
       font-weight: 800;
     }
